7a94ab542d657cd0e0abc11acf6272e58573d97c
Nikolas Tsagkopoulos <n.tsagko@gmail.com>
2019-11-12 16:09:59 +0000

    Finalise SVM.m

--- svm.m
+++ svm.m
@@ -1,6 +1,6 @@
 % SVM.M This script splits the E set in train, test subsets and trains
 % writer dependent SVMs in order to classify between original and forged
-% signatures.
+% signatures. In the last step it tests the trained SVMS on the test set.
 %
 % E set: The dataset describes 10 writers
 %        10x24 original | 10x14 random forgeries | 10x24 skilled forgeries
@@ -12,6 +12,7 @@ load(fullfile(pwd,'data','E_set.mat'));
 
 return_class = @(x) str2double(cell2mat(x));
 
+%% Train, Test assignment
 for c=E.meta.classes
     label = return_class(c) - 1; % labels are zero indexed
     split = 0;
@@ -28,4 +29,41 @@ for c=E.meta.classes
         
     end
     
+end
+
+%% SVM training
+z = E.images.features(strcmp(E.images.identities,'random_forgery'),:);
+rf_idx = (1:14);
+models = cell(numel(E.meta.classes),1);
+for c=E.meta.classes
+    label = return_class(c) - 1;
+    x = E.images.features(E.images.labels' == label & E.images.set == 1,:);
+    
+    X = [x; z(rf_idx,:)];
+    Y = [ones(14,1); zeros(14,1)];
+    
+    SVMModel = fitcsvm(X,Y,'KernelFunction','rbf',...
+    'Standardize',false,'ClassNames',{'1','0'});
+    models{return_class(c)} = SVMModel;
+        
+    rf_idx = rf_idx + 14;
+end
+    
+%% SVM testing
+
+for c=E.meta.classes
+    label = return_class(c) - 1;
+
+    X = E.images.features(E.images.labels' == label & E.images.set == 3,:);
+    Y_true = [ones(10,1); zeros(24,1)];
+    
+    [Y_pred,score] = predict(models{return_class(c)},X);
+    Y_pred = str2num(cell2mat(Y_pred));
+    
+    C = confusionmat(Y_true, Y_pred);
+    results.confusion_matrix{return_class(c)} = C;
+    results.precision(return_class(c)) = C(2,2) / (C(2,2) + C(1,2));
+    results.recall(return_class(c)) = C(2,2) / (C(2,2) + C(2,1));
+    results.f1(return_class(c)) = 2 * results.precision(return_class(c)) * results.recall(return_class(c)) ...
+                                    /(results.precision(return_class(c)) + results.recall(return_class(c)));
 end
